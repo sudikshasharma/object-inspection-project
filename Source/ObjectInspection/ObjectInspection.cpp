c102f670ad13ccfc949fd318053b45b21a6d299c
Sudiksha Sharma <sudiksha.sd1@gmail.com>
2022-12-04 02:14:31 +0100

    - Added Project to git - Created 2d torus mesh (low poly and high poly) and imported them in level (under Content/Models/Custom) - Created emissive material for torus mesh (under Content/Material/Custom)

---
+++ Source/ObjectInspection/ObjectInspection.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ObjectInspection.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ObjectInspection, "ObjectInspection" );
